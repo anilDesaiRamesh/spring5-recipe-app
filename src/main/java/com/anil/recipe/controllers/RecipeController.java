d22ec34a72773796cdadc0aa8020c78e54dae484
Anil_Ramesh <anil.hhh.desai@gmail.com>
2018-11-11 11:04:03 +0000

    Created artificats for creating new recipe. Closes #4

--- src/main/java/com/anil/recipe/controllers/RecipeController.java
+++ src/main/java/com/anil/recipe/controllers/RecipeController.java
@@ -6,10 +6,13 @@ package com.anil.recipe.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.anil.recipe.domain.Recipe;
+import com.anil.recipe.domain.commands.RecipeCommand;
 import com.anil.recipe.services.RecipeService;
 
 /**
@@ -33,6 +36,20 @@ public class RecipeController {
 		Recipe findById = recipeService.findById(new Long(id));
 		model.addAttribute("recipe", findById);
 		return "recipe/show";
+
+	}
+
+	@RequestMapping({"/recipe/new/","recipe/new"})
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+
+	@PostMapping
+	@RequestMapping("/recipe/")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+		RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+		return "redirect:/recipe/show/" + saveRecipeCommand.getId();
 
 	}
 
